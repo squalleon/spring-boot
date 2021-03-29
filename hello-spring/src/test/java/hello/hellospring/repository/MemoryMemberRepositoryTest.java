3e1fd54d5f2c0eec8350b1ee743d3b6977370b82
squalleon <spiritual.awaken@gmail.com>
2021-03-30 00:54:00 +0900

    mod

---
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,24 @@
+package hello.hellospring.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemoryMemberRepositoryTest {
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findByName() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+}
